339e4fcc341729751ba0c70a7eee41384be25961
Adam Akers <Adam.Akers@connellsgroup.co.uk>
2025-10-02 15:38:15 +0100

    favicon and json outline for scores

---
+++ src/pages/data/scoreScheme.js
@@ -0,0 +1,23 @@
+export const formSchema = [
+    {
+        defaultSettings: [
+            {
+                assurance: 5,
+                speed: 5,
+                price: 5,
+            },
+        ],
+        scores: [
+            {
+                question1: {
+                    selected: {
+                        output: {
+                            score: 10,
+                            text: 'High Assurance',
+                        },
+                    },
+                },
+            },
+        ],
+    },
+]
